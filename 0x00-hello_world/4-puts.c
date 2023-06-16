b2af65cf267c70269e30cff616c5b66e974b9cc3
darkweb907 <barryopeyemi@gmail.com>
2023-06-15 17:44:56 -0700

    pull

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point of the program using puts
-* Return: 0 on success
-*/
+*main - Entry point of the program using puts
+*Return: 0 on success;
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
